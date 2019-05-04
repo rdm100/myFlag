0dfd35de68028c1dbdf8bebc3e5c90245bd27ae8
rdm100 <robertode_marco@hotmail.com>
2019-05-04 18:53:11 +0100

    Added api route to server.js and just before started mongod and ran npm start to check if sever connected to database. It did as console message saying Connected to DB

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ MongoClient.connect("mongodb://localhost:27017", (err, client)=>{if (err) next(e
 const db = client.db("flagsdb");
 console.log("Connected to DB");
 
+app.get("/api/flags", (req, res, next) => {
+  const flagsCollection = db.collection("flags");
+  flagsCollection.find().toArray((err, flags) => {
+    if(err) next(err);
+    res.json(flags);
+  });
 
+});
 
 app.listen(port, ()=>{
   console.log("app listening on port", port);
