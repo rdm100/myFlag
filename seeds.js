e9b61920116e4fba8c48f1c108a6c4e858176039
rdm100 <robertode_marco@hotmail.com>
2019-05-08 15:43:25 +0100

    Changes to API

--- seeds.js
+++ seeds.js
@@ -5,14 +5,20 @@ db.flags.remove({});
 db.flags.insert([
 
 {
-"id": 1,
-"name": "Scotland"
-"flagLEDs":{"position": [0, 1], "status": "off"}
+  "id": 1,
+  "name": "Scotland"
+  "flagLEDs":[
+    {"position": [0, 1], "status": "off"},
+    {"position": [0, 2], "status": "off"}
+  ]
 },
 {
-"id": 2,
-"name": "Scotland"
-"flagLEDs":{"position": [0, 2], "status": "off"}
+  "id": 2,
+  "name": "Italy"
+  "flagLEDs":[
+    {"position": [0, 1], "status": "off"},
+    {"position": [0, 2], "status": "off"}
+  ]
 }
 
 ]
